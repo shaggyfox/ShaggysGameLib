b2813fd32174b284097437eac598bceca7d158a3
Bastian Kummer <bastian.kummer@securepoint.de>
2019-04-26 11:04:19 +0200

    add macros for rectangle drawing

--- engine.h
+++ engine.h
@@ -42,5 +42,10 @@ extern struct game_ctx *glob_game_ctx;
 #define draw_line(x1, y1, x2, y2) SDL_RenderDrawLine(glob_renderer, x1, y1, x2, y2)
 #define draw_point(x, y) SDL_RenderDrawPoint(glob_renderer, x, y)
 #define draw_color(r, g, b, a) SDL_SetRenderDrawColor(glob_renderer, r, g, b, a)
+#define draw_rect(rect) SDL_RenderDrawRect(glob_renderer, rect)
+#define draw_rect4(x,y,w,h) SDL_RenderDrawRect(glob_renderer, &(SDL_Rect){x, y, w ,h})
+#define draw_fill_rect(rect) SDL_RenderFillRect(glob_renderer, rect)
+#define draw_fill_rect4(x,y,w,h) SDL_RenderFillRect(glob_renderer, &(SDL_Rect){x, y, w ,h})
+
 void engine_quit(void);
 
