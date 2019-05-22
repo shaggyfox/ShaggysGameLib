b68e6c2a1e1fbda60fdb8d72a1dc1b16444241f9
Bastian Kummer <bastian.kummer@securepoint.de>
2019-05-22 16:23:43 +0200

    some work on tinytest-scheme wrapper

--- tinytest.c
+++ tinytest.c
@@ -1,3 +1,4 @@
+#include "engine.h"
 #include <stdarg.h>
 
 #define USE_INTERFACE 1
@@ -23,46 +24,6 @@ pointer scheme_easy_call(scheme *sc, char *cmd, ...)
   return scheme_eval(sc, eval_cons);
 }
 
-#define MAX_ARGS 12
-static pointer intern_argv_array[12];
-
-
-pointer* scheme_list_to_argv(scheme *sc, pointer args, int *argc_par)
-{
-  int argc = 0;
-  while(args != sc->NIL) {
-    pointer e = sc->vptr->pair_car(args);
-    args = sc->vptr->pair_cdr(args);
-    intern_argv_array[argc++] = e;
-  }
-  if (argc_par) {
-    *argc_par = argc;
-  }
-  return intern_argv_array;
-}
-
-pointer c_function(scheme *sc, pointer args)
-{
-  int argc = 0;
-  pointer *argv = NULL;
-  argv = scheme_list_to_argv(sc, args, &argc);
-  for( int i = 0; i < argc; ++i) {
-    switch (type(argv[i])) {
-      case T_STRING:
-        printf("%s\n", sc->vptr->string_value(argv[i]));
-        break;
-      case T_NUMBER:
-        printf("%.2f\n", sc->vptr->rvalue(argv[i]));
-        break;
-      default:
-        printf("got %p\n", argv[i]);
-        break;
-    }
-  }
-  return sc->NIL;
-}
-
-
 char *scheme_get_string(scheme *sc, pointer *value)
 {
   char * ret = NULL;
@@ -83,7 +44,6 @@ long int scheme_get_integer(scheme *sc, pointer *value)
   if (*value != sc->NIL) {
     pointer v = sc->vptr->pair_car(*value);
     if (sc->vptr->is_integer(v)) {
-      printf("is integer\n");
       ret = sc->vptr->ivalue(v);
     } else if(sc->vptr->is_real(v)) {
       ret = sc->vptr->rvalue(v);
@@ -127,23 +87,76 @@ pointer s_my_test(scheme *sc, pointer args)
   return sc->NIL;
 }
 
+/* -------- API WRAPPER ---------- */
+
+static pointer scheme_clear_screen(scheme *sc, pointer args)
+{
+  clear_screen();
+  return sc->NIL;
+}
+
+static pointer scheme_draw_text(scheme *sc, pointer args)
+{
+  char *text = scheme_get_string(sc, &args);
+  int x = scheme_get_integer(sc, &args);
+  int y = scheme_get_integer(sc, &args);
+  text_print(x, y, text);
+  return sc->NIL;
+}
+
+/* -------- GAME CTX ------------- */
+
+
+static void tinytest_init(void **data_p)
+{
+  scheme *sc = scheme_init_new();
+
+
+  sc->vptr->scheme_define(sc, sc->global_env, sc->vptr->mk_symbol(sc, "clear-screen"), sc->vptr->mk_foreign_func(sc, &scheme_clear_screen));
+  sc->vptr->scheme_define(sc, sc->global_env, sc->vptr->mk_symbol(sc, "draw-text"), sc->vptr->mk_foreign_func(sc, &scheme_draw_text));
+
+
+  scheme_set_output_port_file(sc, stdout);
+  *data_p = sc;
+  FILE *f = fopen("game.scm", "r");
+  scheme_load_file(sc, f);
+  fclose(f);
+}
+
+static void tinytest_update(void* data, float delta)
+{
+  scheme *sc = data;
+  scheme_easy_call(sc, "game_update", mk_real(sc, delta), NULL);
+  scheme_flush_sink(sc);
+}
+
+static void tinytest_draw(void *data)
+{
+  scheme *sc = data;
+  scheme_easy_call(sc, "game_draw", NULL);
+}
+
+
+static struct game_ctx ctx = {
+  .game_init = tinytest_init,
+  .game_update = tinytest_update,
+  .game_draw = tinytest_draw
+};
+
+ENGINE_MAIN(&ctx);
+
+#if 0
 int main()
 {
   scheme *sc = scheme_init_new();
-  sc->vptr->scheme_define(sc, sc->global_env, sc->vptr->mk_symbol(sc, "c_function"), sc->vptr->mk_foreign_func(sc, &c_function));
+  //sc->vptr->scheme_define(sc, sc->global_env, sc->vptr->mk_symbol(sc, "c_function"), sc->vptr->mk_foreign_func(sc, &c_function));
   sc->vptr->scheme_define(sc, sc->global_env, sc->vptr->mk_symbol(sc, "my_test"), sc->vptr->mk_foreign_func(sc, &s_my_test));
   sc->gc_verbose = 1;
   scheme_set_output_port_file(sc, stdout);
   scheme_load_string(sc, "(define value 0) (define (count) (set! value (+ value 1)) (display value) (newline) value)");
   while(1) {
-    scheme_easy_call(sc, "c_function", 
-        mk_string(sc, "blabla"),
-        mk_integer(sc, 1),
-        mk_real(sc, 3.3), NULL);
     scheme_easy_call(sc, "my_test", mk_integer(sc, 3), mk_string(sc, "bla"), NULL);
-    //scheme_apply0(sc, "c_function");
-    //scheme_eval(sc, cmd);
-    //sc->vptr->set_car(sc->sink, sc->NIL);
     scheme_flush_sink(sc);
   }
 }
+#endif
