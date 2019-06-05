9d8e7c18cabdc16ceaab92241ba22e9f26dab148
Bastian Kummer <bastian.kummer@securepoint.de>
2019-06-05 16:49:19 +0200

    Some more work on generic map

--- map.c
+++ map.c
@@ -4,17 +4,21 @@
 
 void *map_get(struct map* map, int x, int y)
 {
-  int chunk_id = (x / map->chunk_size) + (y / map->chunk_size) * map->width;
+  int chunk_id = (x / map->chunk_size) + (y / map->chunk_size) * map->w_chunks;
   struct map_chunk *chunk = &map->chunks[chunk_id];
   return chunk->data[(x % map->chunk_size) + (y % map->chunk_size) * map->chunk_size];
 }
 
 #define map_xy(map, x, y) \
-  map->chunks[(x / map->chunk_size) + (y / map->chunk_size) * map->width].data[x % map->chunk_size + (y % map->chunk_size) * map->chunk_size]
+  map->chunks[(x / map->chunk_size) + (y / map->chunk_size) * map->w_chunks].data[x % map->chunk_size + (y % map->chunk_size) * map->chunk_size]
 
 void map_set(struct map* map, int x, int y, void* map_data)
 {
-  int chunk_id = (x / map->chunk_size) + (y / map->chunk_size) * map->width;
+  if (x < 0 || y < 0 || x >= map->width || y >= map->width) {
+    return;
+  }
+
+  int chunk_id = (x / map->chunk_size) + (y / map->chunk_size) * map->w_chunks;
   struct map_chunk *chunk = &map->chunks[chunk_id];
   chunk->data[(x % map->chunk_size) + (y % map->chunk_size) * map->chunk_size] = map_data;
 }
@@ -22,6 +26,13 @@ void map_set(struct map* map, int x, int y, void* map_data)
 /* cb(int x, int y, void* map_data, void* user_data) */
 void map_foreach(struct map* map, int in_x, int in_y, int in_w, int in_h, void(*cb)(int, int, void*, void*), void* cb_data)
 {
+  if (in_x < 0 || in_y < 0 || in_x >= map->width || in_y >= map->height) {
+    return;
+  }
+  if (in_x + in_w > map->width || in_y + in_h > map->height) {
+    return;
+  }
+
   for (int y = in_y; y < in_y + in_h; ++y) {
     for (int x = in_x; x < in_x + in_w; ++x) {
       cb(x, y, map_xy(map, x, y), cb_data);
@@ -49,10 +60,11 @@ struct map *map_load(char *data, size_t length)
 struct map *map_new(int width, int height)
 {
   struct map *ret = malloc(sizeof(*ret));
-  ret->width = width / 64 + 1;
-  ret->height = height / 64 + 1;
-  ret->chunk_size = 64;
-  ret->chunks = calloc(1, sizeof(*ret->chunks) * ret->width * ret->height);
+  ret->width = width;
+  ret->height = height;
+  ret->chunk_size = MAP_CHUNK_SIZE;
+  ret->w_chunks = (width + (ret->chunk_size - 1)) / ret->chunk_size;
+  ret->chunks = calloc(1, sizeof(*ret->chunks) * ret->w_chunks * ret->h_chunks);
   return ret;
 }
 
--- map.h
+++ map.h
@@ -1,32 +1,43 @@
 #ifndef map_h
 #define map_h
 #include <sys/types.h>
+
+
+#define MAP_CHUNK_SIZE 64
 struct map_chunk {
-  void *data[64 * 64];
+  void *chunk_data;
+  void *data[MAP_CHUNK_SIZE * MAP_CHUNK_SIZE];
 };
 
 struct map {
-  int width; /* in chunks */
-  int height; /* in chunks */
+  int type;
+  int width; /* in tiles */
+  int height; /* in tiles */
+  int w_chunks; /* width in chunks */
+  int h_chunks; /* height in chunks */
   int chunk_size;
   struct map_chunk *chunks;
 };
 
-/* XXX in scheme we only support FRAME. Arbitrary scheme-objects will get
- * flushed by gc */
+int map_type(struct map* map);
 
-// API_CALL map_get MAP INT INT RETURNS_FRAME
 void *map_get(struct map* map, int x, int y);
 
-// API_CALL map_set MAP INT INT FRAME
 void map_set(struct map* map, int x, int y, void* map_data);
 
 struct map *map_load(char *data, size_t length);
-// API_CALL map_new INT INT RETURNS_MAP
+
 struct map *map_new(int width, int height);
 
 void map_free(struct map*);
 
+void map_foreach_chunk(struct map* map, int in_x, int in_y, int in_w, int in_h,
+    void (*cb)(int, int, void*, void*), void *cb_data);
+
+void map_chunk_foreach(struct map_chunk *chunk, int x, int y, int w, int in_h,
+    void (*cb)(int, int, void*, void*), void *cb_data);
+
 /* cb(int x, int y, void* map_data, void* user_data) */
-void map_foreach(struct map* map, int in_x, int in_y, int in_w, int in_h, void(*cb)(int, int, void*, void*), void* cb_data);
+void map_foreach(struct map* map, int in_x, int in_y, int in_w, int in_h,
+    void(*cb)(int, int, void*, void*), void* cb_data);
 #endif
